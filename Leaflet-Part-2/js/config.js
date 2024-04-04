4e28f633e2d26027168471d420aa143245c8fd4b
James Montgomery <145395275+braxtol@users.noreply.github.com>
2024-04-04 09:01:38 -0700

    Update config.js

--- Leaflet-Part-2/js/config.js
+++ Leaflet-Part-2/js/config.js
@@ -1,6 +1,2 @@
 // MAPBOX API KEY
-
-const API_KEY = {
-    key: 'pk.eyJ1IjoiYnJheHRvbCIsImEiOiJjbHVrZHdxYWwwOGlsMmptd3Q4N3drcDQ5In0.jrpZZfLX37JnB1EfwwhLow'
-};
-module.exports = API_KEY;
+const API_KEY = 'pk.eyJ1IjoiYnJheHRvbCIsImEiOiJjbHVrZHdxYWwwOGlsMmptd3Q4N3drcDQ5In0.jrpZZfLX37JnB1EfwwhLow';
